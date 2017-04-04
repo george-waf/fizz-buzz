3b272f77181c4a0a7f6fc3ceeaf83a017339f317
george-waf <georgewafulan@gmail.com>
2017-04-04 16:12:42 +0300

    first commit

---
+++ fizz_buzz_lab.py
@@ -0,0 +1,42 @@
+def fizz_buzz(number):
+    number=int(number)
+    if number %3==0 and number %5==0:
+        return 'FizzBuzz'
+    elif number %5==0:
+        return 'Buzz'
+    elif number %3==0:
+        return 'Fizz'
+    else:
+        return (number)
+      
+    
+
+import unittest        
+class FizzBuzzClassTest(unittest.TestCase):
+    """docstring for FizzBuzz"""
+
+    def test_fizz_1(self):
+        self.assertEqual(fizz_buzz(3), 'Fizz', msg='should return `Fizz` for number divisible by 3')
+    
+    def test_fizz_2(self):
+        self.assertEqual(fizz_buzz(33), 'Fizz', msg='should return `Fizz` for number divisible by 3')
+    
+    def test_buzz_1(self):
+        self.assertEqual(fizz_buzz(5), 'Buzz', msg='should return `Buzz` for number divisible by 5')
+    
+    def test_buzz_2(self):
+        self.assertEqual(fizz_buzz(25), 'Buzz', msg='should return `Buzz` for number divisible by 5')
+
+    def test_fizz_buzz_1(self):
+        self.assertEqual(fizz_buzz(15), 'FizzBuzz', msg='should return `FizzBuzz` for number divisible by 3 and 5')
+    
+    def test_fizz_buzz_2(self):
+        self.assertEqual(fizz_buzz(105), 'FizzBuzz', msg='should return `FizzBuzz` for number divisible by 3 and 5')
+        
+    def test_indivisible_1(self):
+        self.assertEqual(fizz_buzz(101), 101, msg='should return the number if its in divisible by neither 3 or 5')
+        
+    def test_indivisible_2(self):
+        self.assertEqual(fizz_buzz(8), 8, msg='should return the number if its in divisible by neither 3 or 5')
+
+unittest.main(verbosity=2)        
